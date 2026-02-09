bdd01bbfad335a94b08781a896117498c7571faf
Antonio <angelantonioflore837@gmail.com>
2026-02-08 19:01:07 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './' // IMPORTANTE para que funcione en Vercel
+  base: './' // IMPORTANTE para que funcione en Vercel y GitHub Pages
 });
